7029d1a61c04667bfcb67a88c004e34475527660
MO0n <tanjelly@gmail.com>
2020-11-04 11:24:55 +0800

    mod: 删除废弃文件

--- src/wappalyzer/index.js
+++
@@ -1,9 +0,0 @@
-const Driver = require('./driver');
-
-class Wappalyzer {
-    constructor(url, headers, body, rules, options) {
-        return new Driver(url, headers, body, rules, options);
-    }
-}
-
-module.exports = Wappalyzer;
